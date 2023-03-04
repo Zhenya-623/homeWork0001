fda764ecea75607624c836fda9dae567c28d6edb
Евгений Политыко <Zhenya-623@internet.ru>
2023-03-04 22:14:11 +0300

    first task is done

--- src/main/java/org/example/cw1/Ex1.java
+++ src/main/java/org/example/cw1/Ex1.java
@@ -1,4 +1,15 @@
 package org.example.cw1;
 
+import java.util.Scanner;
+
 public class Ex1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        str = str.trim();
+        String [] array = str.split(" ");
+        for (int i = array.length-1; i < array.length; i--) {
+            System.out.println(array[i]);
+        }
+    }
 }
